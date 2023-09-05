5e9b905e0eb0aa21ee7aeca43d8df921765be6a9
Tim Harding <tim@timharding.co>
2023-09-05 15:36:20 -0700

    Next lesser part of next number

--- src/problems/_05_bit_manipulation/_04_next_number.rs
+++ src/problems/_05_bit_manipulation/_04_next_number.rs
@@ -28,6 +28,7 @@ mod tests {
 
     #[test]
     fn returns_original_for_no_greater() {
+        assert_eq!(Solution::next_greater(0b0), 0b0);
         assert_eq!(Solution::next_greater(u64::MAX), u64::MAX);
         assert_eq!(Solution::next_greater(u64::MAX - 1), u64::MAX - 1);
         assert_eq!(Solution::next_greater(u64::MAX - 3), u64::MAX - 3);
@@ -35,8 +36,9 @@ mod tests {
 
     #[test]
     fn returns_original_for_no_lesser() {
-        assert_eq!(Solution::next_lesser(0), 0);
+        assert_eq!(Solution::next_lesser(0b0), 0b0);
         assert_eq!(Solution::next_lesser(0b111), 0b111);
+        assert_eq!(Solution::next_lesser(u64::MAX), u64::MAX);
     }
 
     #[test]
@@ -50,6 +52,9 @@ mod tests {
         assert_eq!(Solution::next_greater(0b10101), 0b10110);
         assert_eq!(Solution::next_greater(0b100111), 0b101011);
         assert_eq!(Solution::next_greater(0b100111000), 0b101000011);
+        assert_eq!(Solution::next_greater(0b10010011), 0b10010101);
+        assert_eq!(Solution::next_greater(0b100110011), 0b100110101);
+        assert_eq!(Solution::next_greater(0b1000011), 0b1000101);
     }
 
     #[test]
@@ -62,6 +67,9 @@ mod tests {
         assert_eq!(Solution::next_lesser(0b10011), 0b1110);
         assert_eq!(Solution::next_lesser(0b10101), 0b10011);
         assert_eq!(Solution::next_lesser(0b100111), 0b11110);
-        assert_eq!(Solution::next_lesser(0b100111000), 0b10011010);
+        assert_eq!(Solution::next_lesser(0b100111000), 0b100110100);
+        assert_eq!(Solution::next_lesser(0b10010011), 0b10001110);
+        assert_eq!(Solution::next_lesser(0b100110011), 0b100101110);
+        assert_eq!(Solution::next_lesser(0b1000011), 0b111000);
     }
 }
--- src/solutions/_05_bit_manipulation/_04_next_number.rs
+++ src/solutions/_05_bit_manipulation/_04_next_number.rs
@@ -4,12 +4,21 @@ pub struct Solution;
 
 impl NextNumber for Solution {
     fn next_lesser(n: u64) -> u64 {
-        todo!()
+        let ones = n.trailing_ones();
+        let zeroes = n.checked_shr(ones).unwrap_or(0).trailing_zeros();
+        let total = ones + zeroes;
+        if total >= 64 {
+            n
+        } else {
+            let mask = u64::MAX.checked_shl(total + 1).unwrap_or(0);
+            let new_leading = !u64::MAX.checked_shl(ones + 1).unwrap_or(0) << (zeroes - 1);
+            (n & mask) | new_leading
+        }
     }
 
     fn next_greater(n: u64) -> u64 {
         let zeroes = n.trailing_zeros();
-        let ones = (n >> zeroes).trailing_ones();
+        let ones = n.checked_shr(zeroes).unwrap_or(0).trailing_ones();
         let total = zeroes + ones;
         if total >= 64 {
             n
@@ -17,7 +26,6 @@ impl NextNumber for Solution {
             let mask = u64::MAX << total;
             let shifted_bit = 1 << total;
             let new_carried = !(u64::MAX << (ones - 1));
-            println!("{zeroes}, {ones}, {mask:b}, {shifted_bit:b}, {new_carried:b}");
             (n & mask) | shifted_bit | new_carried
         }
     }
