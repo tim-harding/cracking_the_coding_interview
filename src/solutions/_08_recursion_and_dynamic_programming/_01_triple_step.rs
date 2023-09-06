740ba84feb768d8241b5e2bfe6f357f645e59bdd
Tim Harding <tim@timharding.co>
2023-09-05 17:55:06 -0700

    Simplified basic solution

--- src/solutions/_08_recursion_and_dynamic_programming/_01_triple_step.rs
+++ src/solutions/_08_recursion_and_dynamic_programming/_01_triple_step.rs
@@ -4,27 +4,19 @@ pub struct Solution;
 
 impl TripleStep for Solution {
     fn triple_step(steps: usize) -> usize {
-        match steps {
-            0 => 1,
-            1 => 1,
-            2 => 2,
-            3 => 4,
-            _ => {
-                // This counts the number of ways to get to each stair. Include two
-                // dummy stairs to avoid out-of-bounds indexing.
-                let mut stairs = vec![0; steps];
-                stairs[0] = 1;
-                stairs[1] = 2;
-                stairs[2] = 4;
-                for i in 3..steps {
-                    // There is one way to get from the previous stair, two ways (11, 2)
-                    // to get from the stair two down, and four ways (111, 21, 12, 3) to
-                    // get from the stair three down.
-                    stairs[i] = stairs[i - 1] + stairs[i - 2] + stairs[i - 3];
-                }
-                stairs[steps - 1]
-            }
+        // This counts the number of ways to get to each stair. Include two
+        // dummy stairs to avoid out-of-bounds indexing.
+        let mut stairs = vec![0; steps.max(3)];
+        stairs[0] = 1;
+        stairs[1] = 2;
+        stairs[2] = 4;
+        for i in 3..steps {
+            // There is one way to get from the previous stair, two ways (11, 2)
+            // to get from the stair two down, and four ways (111, 21, 12, 3) to
+            // get from the stair three down.
+            stairs[i] = stairs[i - 1] + stairs[i - 2] + stairs[i - 3];
         }
+        stairs[steps - 1]
     }
 }
 
